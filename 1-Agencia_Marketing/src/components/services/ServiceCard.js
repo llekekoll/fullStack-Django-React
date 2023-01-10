474d4eac4c0302a266e218971a9aa9317adcd3ae
ekeko <cemartia@hotmail.com>
2023-01-09 21:34:02 -0300

    update 09 01 2023

---
+++ 1-Agencia_Marketing/src/components/services/ServiceCard.js
@@ -0,0 +1,10 @@
+function ServiceCard() {
+    return (
+        <div className="W-full p-8 h-96 lg:h-[150-pm] bg-white hover:-translate-y-1 transition duration-300 ease-in-out">
+            <div className="w-full">
+                <i className='bx bx-code-block text-6xl text-gray-500'></i>
+            </div>
+        </div>
+    )
+}
+export default ServiceCard
--- 1-Agencia_Marketing/src/containers/pages/Home.jsx
+++ 1-Agencia_Marketing/src/containers/pages/Home.jsx
@@ -7,7 +7,7 @@ import UseCases from "components/home/UseCases"
 import Footer from "components/navigation/Footer"
 import Navbar from "components/navigation/Navbar"
 import Layout from "hocs/layouts/Layout"
-import LogoCloud from "./LogoCloud"
+import LogoCloud from "../../components/home/LogoCloud"
 
 function Home() {
     return (

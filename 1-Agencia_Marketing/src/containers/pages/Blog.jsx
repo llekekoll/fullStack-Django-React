ac77505a0063bc27a0246cfa9b973025917faeaf
ekeko <cemartia@hotmail.com>
2023-01-10 22:54:41 -0300

    update

--- 1-Agencia_Marketing/src/containers/pages/Blog.jsx
+++ 1-Agencia_Marketing/src/containers/pages/Blog.jsx
@@ -10,32 +10,32 @@ function Home() {
     }, [])
     return (
         <Layout>
-              <Helmet>
-        <title>Fork DAO | Blog</title>
-        <meta name="description" content="Estableciendonos como un agregador web3." />
-        <meta name="keywords" content='web3, DAO, integraciones, descentralizacion' />
-        <meta name="robots" content='all' />
-        <link rel="canonical" href="https://www.fork-dao.com/" />
-        <meta name="author" content='ekeko' />
-        <meta name="publisher" content='ekeko' />
+            <Helmet>
+                <title>Fork DAO | Blog</title>
+                <meta name="description" content="Estableciendonos como un agregador web3." />
+                <meta name="keywords" content='web3, DAO, integraciones, descentralizacion' />
+                <meta name="robots" content='all' />
+                <link rel="canonical" href="https://www.fork-dao.com/" />
+                <meta name="author" content='ekeko' />
+                <meta name="publisher" content='ekeko' />
 
-        {/* Social Media Tags */}
-        <meta property="og:title" content='Fork DAO | "todo roto"' />
-        <meta property="og:description" content='Estableciendonos como un agregador web3.' />
-        <meta property="og:url" content="www.fork-dao.com/" />
-        <meta property="og:image" content='https://bafybeigh4em54oo3stszmrfh756dilog63eszlwhkwpnst3xxzpmwbctxm.ipfs.w3s.link/FDNEGROCIRCULAR.png' />
+                {/* Social Media Tags */}
+                <meta property="og:title" content='Fork DAO | "todo roto"' />
+                <meta property="og:description" content='Estableciendonos como un agregador web3.' />
+                <meta property="og:url" content="www.fork-dao.com/" />
+                <meta property="og:image" content='https://bafybeigh4em54oo3stszmrfh756dilog63eszlwhkwpnst3xxzpmwbctxm.ipfs.w3s.link/FDNEGROCIRCULAR.png' />
 
-        <meta name="twitter:title" content='Fork DAO | "todo roto"' />
-        <meta
-            name="twitter:description"
-            content='Estableciendonos como un agregador web3.'
-        />
-        <meta name="twitter:image" content='https://bafybeigh4em54oo3stszmrfh756dilog63eszlwhkwpnst3xxzpmwbctxm.ipfs.w3s.link/FDNEGROCIRCULAR.png' />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Helmet>
+                <meta name="twitter:title" content='Fork DAO | "todo roto"' />
+                <meta
+                    name="twitter:description"
+                    content='Estableciendonos como un agregador web3.'
+                />
+                <meta name="twitter:image" content='https://bafybeigh4em54oo3stszmrfh756dilog63eszlwhkwpnst3xxzpmwbctxm.ipfs.w3s.link/FDNEGROCIRCULAR.png' />
+                <meta name="twitter:card" content="summary_large_image" />
+            </Helmet>
             <Navbar />
             <div className="pt-28">
-                Home
+                Blog
             </div>
             <Footer />
         </Layout>
